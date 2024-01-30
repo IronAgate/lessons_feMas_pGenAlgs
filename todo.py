c92e5b784506f0c6d277805ccfcca30b7a583f68
IronAgate <ironagate@gmail.com>
2024-01-30 16:07:03 -0600

    added todo notes

---
+++ todo.py
@@ -0,0 +1,22 @@
+
+
+small exercises from this lesson that might be beneficial to do at some point:
+
+
+class not_yet_done
+	
+	Heap code which can be given a function/comparison, like qsort, and be a min or max heap
+	
+	quicksort function which can be given a function/comparison, like qsort
+
+	implementing tree stuff
+		BST
+		DFS
+		BFS
+	
+	recursion-less sqrt(n) exercise
+
+
+class not_not_yet_done
+	
+	recursion-less quicksort
